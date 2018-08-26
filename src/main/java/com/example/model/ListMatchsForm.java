8db35d50099de93bc97b0f82875948b43ce2320c
Manuel <mjbeltrangarcia@gmail.com>
2018-08-26 20:38:31 +0200

    test

---
+++ src/main/java/com/example/model/ListMatchsForm.java
@@ -0,0 +1,16 @@
+package com.example.model;
+
+import java.util.List;
+
+public class ListMatchsForm {
+
+	private List<Match> listMatchs;
+
+	public List<Match> getListMatchs() {
+		return listMatchs;
+	}
+
+	public void setListMatchs(List<Match> listMatchs) {
+		this.listMatchs = listMatchs;
+	}
+}
